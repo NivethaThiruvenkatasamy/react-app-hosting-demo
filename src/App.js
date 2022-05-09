85f2f85f7393e42b016e0577948f19574b3b4a10
Nivetha.Thiruvenkatasamy <nivetha.thiruvenkatasamy@NivethaThiruvenkatasamys-MacBook-Pro.local>
2022-05-09 09:57:25 +0800

    dep to s3-13

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import './App.css';
 
 function App() {
 
-  function fetch(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://reqres.in/api/products/3", true);
-    xhr.onload = function(){
-    console.log(xhr.responseText);
-  };
-  xhr.send(); 
-  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sample App to test hosting on S3 + Added github actions+ secutity test +test"
+          Sample App to test hosting on S3 + Added github actions+ secutity test"
         </p>
-        <button onClick={fetch}>
-           click to fetch
-        </button>
+     
       </header>
     </div>
   );
